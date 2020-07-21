480353a6b006fe94fdcf1ea62b65353fda0f4210
LDS_XiaoYe <53298022+LDS-XiaoYe@users.noreply.github.com>
2020-07-21 09:10:59 +0800

    add smoothScroll and so on

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,18 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      ['/', '欢迎页'],
+    ],
+    lastUpdated: '最后更新 ',
+    repo: 'LDS-XiaoYe/myNetlify',
+    docsBranch: 'master',
+    docsDir: 'docs',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '帮助我们完善这个页面',
+    smoothScroll: true
+  },
+  evergreen: true
 }
